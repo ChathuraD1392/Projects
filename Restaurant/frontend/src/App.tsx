b31ec97b50c6ac0d511ea98b927516fcf341a2bc
ChathuraD1392 <chathuradilshan1392@gmail.com>
2025-10-06 07:55:21 +0530

    create home page

--- Restaurant/frontend/src/App.tsx
+++ Restaurant/frontend/src/App.tsx
@@ -1,9 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import NavBar from "./components/navbar/NavBar";
+import Home from "./assets/Home/Home";
+import Cart from "./assets/Cart/Cart";
+import Order from "./assets/Order/Order";
 
 function App() {
   return (
     <>
-      <div>Test</div>
+      <div className="app">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<Order />} />
+        </Routes>
+      </div>
     </>
   );
 }
---
+++ Restaurant/frontend/src/components/Header/Header.tsx
@@ -0,0 +1,25 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <div className="header-contents">
+          <h2>Order your favourites here...</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            cupiditate labore dolorum nulla libero consectetur perspiciatis, ea
+            expedita fuga maxime incidunt totam aut assumenda voluptatibus vero
+            laudantium hic blanditiis omnis, beatae odio ad consequatur.
+            Repellendus magni eaque consectetur aspernatur perferendis labore
+            quam. Autem exercitationem deserunt facere itaque blanditiis
+            reprehenderit? Blanditiis.
+          </p>
+          <button>View Menu</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
