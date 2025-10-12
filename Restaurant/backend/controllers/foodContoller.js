94357ac3c304c844373e5f3c7469e26540e24a31
ChathuraD1392 <chathuradilshan1392@gmail.com>
2025-10-12 21:40:14 +0530

    create api for food items

---
+++ Restaurant/backend/controllers/foodContoller.js
@@ -0,0 +1,57 @@
+import foodModel from "../models/foodModel.js";
+import fs from "fs";
+
+// add a food item
+
+const addItem = async (req, res) => {
+  let image_filename = `${req.file.filename}`;
+
+  const food = new foodModel({
+    name: req.body.name,
+    description: req.body.description,
+    image: image_filename,
+    price: req.body.price,
+    category: req.body.category,
+  });
+
+  try {
+    await food.save();
+    res.json({ success: true, message: "Food Item added Successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+// list food items
+
+const listFood = async (req, res) => {
+  try {
+    const foods = await foodModel.find({});
+    res.json({ success: true, data: foods });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+// remove food item
+const removeItem = async (req, res) => {
+  try {
+    let food = await foodModel.findById(req.body.id);
+    fs.unlink(`uploads/${food.image}`, (err) => {
+      if (err) {
+        console.log("Path is not correct", err);
+      } else {
+        console.log("image removed");
+      }
+    });
+
+    await foodModel.findByIdAndDelete(req.body.id);
+    res.json({ success: true, message: "Food item removed successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+export { addItem, listFood, removeItem };
